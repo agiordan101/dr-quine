38ee44e97882e14bbcabf42f6add51a417fc80af
faneyer101 <faneyer@student.42lyon.fr>
2021-04-22 16:05:13 +0200

    almost clean Sully

--- Colleen/Colleen.c
+++ Colleen/Colleen.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
-void	clone(char *code, char el, char quote)
+void	clone(char *code, char eol, char quote)
 {
-	printf(code, el, el, el, el, el, el, el, el, el, el, quote, code, quote, el, el, el, el, el, el);
+	printf(code, eol, eol, eol, eol, eol, eol, eol, eol, eol, eol, quote, code, quote, eol, eol, eol, eol, eol, eol);
 }
 /*
 	Comme en Terre
 */
 int		main()
 {
-	char *code = "#include <stdio.h>%cvoid	clone(char *code, char el, char quote)%c{%c	printf(code, el, el, el, el, el, el, el, el, el, el, quote, code, quote, el, el, el, el, el, el);%c}%c/*%c	Comme en Terre%c*/%cint		main()%c{%c	char *code = %c%s%c;%c	/*%c		3.14%c	*/%c	clone(code, 10, 34);%c}%c";
+	char *code = "#include <stdio.h>%cvoid	clone(char *code, char eol, char quote)%c{%c	printf(code, eol, eol, eol, eol, eol, eol, eol, eol, eol, eol, quote, code, quote, eol, eol, eol, eol, eol, eol);%c}%c/*%c	Comme en Terre%c*/%cint		main()%c{%c	char *code = %c%s%c;%c	/*%c		3.14%c	*/%c	clone(code, 10, 34);%c}%c";
 	/*
 		3.14
 	*/
--- Grace/Grace.c
+++ Grace/Grace.c
@@ -1,8 +1,8 @@
 # include <stdio.h>
 # include <fcntl.h>
-# define CODE "# include <stdio.h>%c# include <fcntl.h>%c# define CODE %c%s%c%c# define FD open(%cGrace_kid.c%c, O_CREAT | O_TRUNC | O_RDWR, 0777)%c# define CLONE(el, quote) int main(){dprintf(FD, CODE, el, el, quote, CODE, quote, el, quote, quote, el, el, el, el, el, el);}%c/*%c	Comment t'es ? Rrr... :)%c*/%cCLONE(10, 34)%c"
+# define CODE "# include <stdio.h>%c# include <fcntl.h>%c# define CODE %c%s%c%c# define FD open(%cGrace_kid.c%c, O_CREAT | O_TRUNC | O_RDWR, 0777)%c# define CLONE(eol, quote) int main(){dprintf(FD, CODE, eol, eol, quote, CODE, quote, eol, quote, quote, eol, eol, eol, eol, eol, eol);}%c/*%c	Comment t'es ? Rrr... :)%c*/%cCLONE(10, 34)%c"
 # define FD open("Grace_kid.c", O_CREAT | O_TRUNC | O_RDWR, 0777)
-# define CLONE(el, quote) int main(){dprintf(FD, CODE, el, el, quote, CODE, quote, el, quote, quote, el, el, el, el, el, el);}
+# define CLONE(eol, quote) int main(){dprintf(FD, CODE, eol, eol, quote, CODE, quote, eol, quote, quote, eol, eol, eol, eol, eol, eol);}
 /*
 	Comment t'es ? Rrr... :)
 */
