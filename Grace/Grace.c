8216a63c47ff7755b71ec3a9c5104d76d272fa9f
faneyer101 <faneyer@student.42lyon.fr>
2021-04-22 15:26:44 +0200

    Clean Grace

--- Grace/Grace.c
+++ Grace/Grace.c
@@ -1,7 +1,8 @@
 # include <stdio.h>
-# define CODE "# include <stdio.h>%c# define CODE %c%s%c%c# define FD fopen(%cGrace_kid.c%c, %cw%c)%c# define CLONE(el, quote) int main(){fprintf(FD, CODE, el, quote, CODE, quote, el, quote, quote, quote, quote, el, el, el, el, el, el);}%c/*%c	Comment t'es ? Rrr... :)%c*/%cCLONE(10, 34)%c"
-# define FD fopen("Grace_kid.c", "w")
-# define CLONE(el, quote) int main(){fprintf(FD, CODE, el, quote, CODE, quote, el, quote, quote, quote, quote, el, el, el, el, el, el);}
+# include <fcntl.h>
+# define CODE "# include <stdio.h>%c# include <fcntl.h>%c# define CODE %c%s%c%c# define FD open(%cGrace_kid.c%c, O_CREAT | O_TRUNC | O_RDWR, 0777)%c# define CLONE(el, quote) int main(){dprintf(FD, CODE, el, el, quote, CODE, quote, el, quote, quote, el, el, el, el, el, el);}%c/*%c	Comment t'es ? Rrr... :)%c*/%cCLONE(10, 34)%c"
+# define FD open("Grace_kid.c", O_CREAT | O_TRUNC | O_RDWR, 0777)
+# define CLONE(el, quote) int main(){dprintf(FD, CODE, el, el, quote, CODE, quote, el, quote, quote, el, el, el, el, el, el);}
 /*
 	Comment t'es ? Rrr... :)
 */
